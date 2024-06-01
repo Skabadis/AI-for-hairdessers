31bbc0392619f0713138ebc1689400303ba81f17
unknown <skandere.sahli@gmail.com>
2024-06-01 12:49:41 -0400

    Format logs better

--- src/utils/logging_config.py
+++ src/utils/logging_config.py
@@ -8,15 +8,16 @@ if not os.path.exists('logs'):
     os.makedirs('logs')
 
 # Create a unique log file name based on the current date and time
-current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-log_filename = f"logs/flask_app_{current_time}.log"
+current_time = datetime.now().strftime("%Y-%m-%d__%H-%M-%S")
+log_filename = f"logs/call_{current_time}.log"
 
 # Set up the log handler
 handler = RotatingFileHandler(log_filename, maxBytes=10000, backupCount=10)
 handler.setLevel(logging.INFO)
 
 # Set up the formatter
-formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+formatter = logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]', datefmt='%H:%M:%S')
 handler.setFormatter(formatter)
 
 # Get the root logger and configure it
