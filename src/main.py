7789bf0c6a1d567f21cc7917a270ed73c68e5050
unknown <skandere.sahli@gmail.com>
2024-06-02 10:32:38 -0400

    Try to fix the hang up problem by handling no user_input better

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ from twilio.rest import Client
 from llms_connectors.openai_connector import get_openai_client
 from utils.read_params import read_params
 from conversation.text_to_text import agentic_answer
-from dotenv import load_dotenv
 import os
 import signal
 # Runs logging_config.py file which sets up the logs
@@ -37,13 +36,14 @@ def voice():
 
     try:
         user_input = request.form.get('SpeechResult')
-
-        if not user_input:  # If no user input, provide the initial response
-            Sandra_response = parameters['discussion']['welcome_message']
-            conversation_history.append(
-                {"role": "assistant", "content": Sandra_response})
+        app.logger.info(f"User said: {user_input}")
+        if not user_input:  # If no user input, ask to repeat
+            gather_fallback = Gather(
+                input='speech', action='/voice', speechTimeout='auto', language='fr-FR')
+            gather_fallback.say("Je n'ai pas entendu votre réponse, pouvez-vous répétez s'il-vous-plait?",
+                                voice='Polly.Lea-Neural', language='fr-FR')
+            resp.append(gather_fallback)
         else:
-            app.logger.info(f"User said: {user_input}")
             conversation_history.append(
                 {"role": "user", "content": user_input})
             Sandra_response = agentic_answer(
@@ -71,29 +71,3 @@ def voice():
                  language='fr-FR')  # use alice to save cost, Polly.Lea-Neural for the best one
 
     return str(resp)
-
-# if __name__ == "__main__":
-    # if not os.path.exists('logs'):
-    #   os.makedirs('logs')
-    # current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    # log_filename = f"logs/call_{current_time}.log"
-
-    # # Configure logging
-    # logging.basicConfig(filename=log_filename,
-    #                     level=logging.INFO,
-    #                     format='%(asctime)s %(levelname)s: %(message)s',
-    #                     datefmt='%Y-%m-%d %H:%M:%S')
-
-    # try:
-    #     app.logger.info("Parameters loaded successfully")
-
-    #     # Load Twilio credentials from .env
-    #     load_dotenv()
-    #     account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-    #     auth_token = os.getenv('TWILIO_AUTH_TOKEN')
-    #     app.logger.info(f"Twilio SID: {account_sid}")
-
-    #     client = Client(account_sid, auth_token)
-    #     app.run(debug=True, host='0.0.0.0', port=8000)
-    # except Exception as e:
-    #     app.logger.error(f"Failed to start the application: {e}")
