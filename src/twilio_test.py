77a36ea344368ae713b186182e636ff15d1a5ba9
unknown <skandere.sahli@gmail.com>
2024-06-08 12:10:48 -0400

    Added credentials in Twilio http request to get recording. Removed beep from record

--- src/twilio_test.py
+++ src/twilio_test.py
@@ -1,10 +1,16 @@
+import speech_recognition as sr
+from io import BytesIO
+import requests
 from flask import Flask, request
 from twilio.twiml.voice_response import VoiceResponse, Gather, Record, Say
 # Runs logging_config.py file which sets up the logs - do not remove
 from utils.logging_config import initialize_logger
 import logging
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+
 
 @app.route("/initialize", methods=['GET', 'POST'])
 def initialize():
@@ -14,11 +20,12 @@ def initialize():
         # Prepare initial response with Gather to start the conversation
     resp = VoiceResponse()
     record = Record(input='speech', action='/voice', speechTimeout='auto',
-                    language='fr-FR', actionOnEmptyResult=True)
+                    language='fr-FR', actionOnEmptyResult=True, playBeep=False)
     answer = Say("Ceci est un test", voice='alice', language='fr-FR')
     resp.append(answer)
     resp.append(record)
     return str(resp)
+
 
 @app.route("/voice", methods=['GET', 'POST'])
 def voice():
@@ -32,20 +39,22 @@ def voice():
         logging.info("No recording URL")
     resp = VoiceResponse()
     record = Record(input='speech', action='/voice', speechTimeout='auto',
-                    language='fr-FR', actionOnEmptyResult=True)
+                    language='fr-FR', actionOnEmptyResult=True, playBeep=False)
     answer = Say("Ceci est un test", voice='alice', language='fr-FR')
     resp.append(answer)
     resp.append(record)
     return str(resp)
 
 
-import requests
-from io import BytesIO
-import speech_recognition as sr
-
 # Function to transcribe audio from Twilio
+
 def transcribe_twilio_audio(recording_url):
-    response = requests.get(recording_url)
+    load_dotenv()
+    # Access API key
+    twilio_account_sid, twilio_auth_token = os.getenv(
+        'TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_AUTH_TOKEN')
+    response = requests.get(recording_url, auth=(
+        twilio_account_sid, twilio_auth_token))
     if response.status_code == 200:
         audio_data = response.content
         recognizer = sr.Recognizer()
@@ -62,7 +71,8 @@ def transcribe_twilio_audio(recording_url):
     else:
         return f"Error retrieving audio file from Twilio: {response.status_code}"
 
-# Usage
-recording_url = "https://api.twilio.com/2010-04-01/Accounts/{AccountSid}/Recordings/{RecordingSid}"
-text = transcribe_twilio_audio(recording_url)
-print("Transcribed text:", text)
+
+# # Usage
+# recording_url = "https://api.twilio.com/2010-04-01/Accounts/{AccountSid}/Recordings/{RecordingSid}"
+# text = transcribe_twilio_audio(recording_url)
+# print("Transcribed text:", text)
