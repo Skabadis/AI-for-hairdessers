8c72511d1b85d51394b996e804ae58b124a4e890
unknown <skandere.sahli@gmail.com>
2024-06-15 19:39:56 -0400

    Add uploading to S3

--- src/twilio_test_streaming.py
+++ src/twilio_test_streaming.py
@@ -5,6 +5,10 @@ import websockets
 import logging
 from utils.logging_config import initialize_logger
 from conversation.recording import initiate_call_recording
+from db_connectors.s3_interactions import upload_log_to_s3, upload_content_to_s3
+from workers.shutdown_worker import shutdown_worker
+from conversation.audio_processing import url_wav_to_audio_file
+
 
 app = Flask(__name__)
 
@@ -33,6 +37,36 @@ def initialize():
         )
     )
     return str(response)
+
+
+@app.route("/call-status", methods=['POST'])
+def call_status():
+    call_status = request.values.get('CallStatus')
+    if call_status in ['completed', 'canceled', 'no-answer']:
+        logging.info(f"Call status: {call_status}")
+
+        # Upload recording to S3
+        _, recording_content = url_wav_to_audio_file(recording_url)
+        content_folder, bucket_name = parameters["paths"]["logs_recording"], parameters["paths"]["s3_bucket_name"]
+        recording_filename = log_filename.replace(".log", ".wav")
+        upload_content_to_s3(recording_content, recording_filename,
+                             content_folder, bucket_name)
+
+        # Shutdown the worker at the end of the call
+        shutdown_worker()
+
+        # Upload the log file to S3
+        log_folder, bucket_name = parameters["paths"]["logs_info"], parameters["paths"]["s3_bucket_name"]
+        upload_log_to_s3(log_filename, log_folder, bucket_name)
+    return ('', 204)
+
+@app.route("/recording-events", methods=['POST'])
+def recording_events():
+    global recording_url
+    # Retrieve recording URL and other details from Twilio's POST request
+    recording_url = request.form['RecordingUrl'] + ".wav"
+    logging.info(f"Recording URL: {recording_url}")
+    return "", 200
 
 async def handle_stream(websocket, path):
     logging.info("Entered handle_stream")
